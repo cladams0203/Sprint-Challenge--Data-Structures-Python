76223c69eee00bf33a5d2b8a5328a63c814aadf6
cladams0203 <cladams0203@gmail.com>
2020-07-31 21:33:07 -0400

    finished names file runtime with BST is .129

--- names/names.py
+++ names/names.py
@@ -22,6 +22,56 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# -----------------my improved solution--------------
+start_time2 = time.time()
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+    def insert(self, value):
+        if self.value > value:
+            if self.left == None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        if self.value <= value:
+            if self.right == None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+    def contains(self, target):
+        if (self.value == target):
+            return True
+        if (self.value > target):
+            if (self.left == None):
+                return False
+            if (self.left.value == target):
+                return True
+            else:
+                return self.left.contains(target)
+        if (self.value <= target):
+            if (self.right == None):
+                return False
+            if (self.right.value == target):
+                return True
+            else:
+                return self.right.contains(target)
+
+name_tree = BSTNode(names_1[0])
+duplicates2 = []
+for i in names_1:
+    name_tree.insert(i)
+
+for i in names_2:
+    if name_tree.contains(i):
+        duplicates2.append(i)
+
+end_time2 = time.time()
+print (f"{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
+print (f"runtime: {end_time2 - start_time2} seconds")
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
